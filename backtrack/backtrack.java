e9f1c49334027ce345667acd98a40efe47466300
VinugnaK <152588912+VinugnaK@users.noreply.github.com>
2025-10-10 16:41:16 +0530

    Update backtrack.java
    part1 completed n queens

--- backtrack/backtrack.java
+++ backtrack/backtrack.java
@@ -45,6 +45,28 @@ class backtrack{
 
         }
     }
+    public static boolean issafe(char board[][],int row,int col){
+        //vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        // diagonal left up
+for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+    if (board[i][j] == 'Q') {
+        return false;
+}}
+
+// diagonal right up
+for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+    if (board[i][j] == 'Q') {
+        return false;
+}}    
+        
+        return true;
+    }
+
 
     //nqueens
     public static void nqueens(char board[][],int row){
@@ -54,14 +76,16 @@ class backtrack{
             return;
         }
         //column loop
+        
         for(int i=0;i<board.length;i++){
+          if(issafe(board,row,i)){
           board[row][i]='Q';
           nqueens(board, row+1);
-          board[row][i]='.';
+          board[row][i]='*';
 
-        }
+        }}
     }
-
+    
     public static void printboard(char board[][]){
         System.out.println("----------chess board---------");
         for(int i=0;i<board.length;i++){
@@ -83,12 +107,12 @@ class backtrack{
          permu(str, "");
 
          //nqueens
-         int n=2;
+         int n=4;
          char board[][]=new char[n][n];
          //intitialize
          for(int i=0;i<n;i++){
              for(int j=0;j<n;j++){
-                board[i][j]='.';
+                board[i][j]='*';
              }
          }
          nqueens(board, 0);
