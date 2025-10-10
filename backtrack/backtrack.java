fa390b365e331f3f2667919f4248fe07d4fe6a1b
VinugnaK <152588912+VinugnaK@users.noreply.github.com>
2025-10-10 12:07:40 +0530

    Update backtrack.java
    finding subsets of strings using backtracking,but here time complexity is more nearlyO(n*2^n)

--- backtrack/backtrack.java
+++ backtrack/backtrack.java
@@ -16,10 +16,26 @@ class backtrack{
         }
         System.out.println();
     }
+
+    //find subets
+    public static void subsets(String str,String ans,int i){
+        //basecase
+        if(i==str.length()){ 
+            if(ans.length()==0){System.out.println("null");}
+            System.out.println(ans);
+            return;}
+        //yes choice
+        subsets(str, ans+str.charAt(i), i+1);
+        //no choice
+        subsets(str, ans, i+1);
+    }
     public static void main(String args[]){
         int arr[]=new int[5];
          arrba(arr, 0, 1);
          printarr(arr);
+         //subsets
+         String str="abc";
+         subsets(str, "", 0);
 
     }
 }
