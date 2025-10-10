d9bdd360d9bb249e25675ff02a2d254c02eef362
VinugnaK <152588912+VinugnaK@users.noreply.github.com>
2025-10-10 15:35:20 +0530

    Update backtrack.java
    nqueens...part1---only
    
    n queens n rows

--- backtrack/backtrack.java
+++ backtrack/backtrack.java
@@ -45,6 +45,32 @@ class backtrack{
 
         }
     }
+
+    //nqueens
+    public static void nqueens(char board[][],int row){
+        //base
+        if(row==board.length){
+            printboard(board);
+            return;
+        }
+        //column loop
+        for(int i=0;i<board.length;i++){
+          board[row][i]='Q';
+          nqueens(board, row+1);
+          board[row][i]='.';
+
+        }
+    }
+
+    public static void printboard(char board[][]){
+        System.out.println("----------chess board---------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+               System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String args[]){
         int arr[]=new int[5];
          arrba(arr, 0, 1);
@@ -55,6 +81,17 @@ class backtrack{
 
          //permutation
          permu(str, "");
+
+         //nqueens
+         int n=2;
+         char board[][]=new char[n][n];
+         //intitialize
+         for(int i=0;i<n;i++){
+             for(int j=0;j<n;j++){
+                board[i][j]='.';
+             }
+         }
+         nqueens(board, 0);
 
     }
 }
