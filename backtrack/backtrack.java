3fc4f4c67bc10a1ceefe8112c6beee833bbcb2e3
VinugnaK <152588912+VinugnaK@users.noreply.github.com>
2025-10-10 12:18:28 +0530

    Update backtrack.java
    permutation--the possibilities of that string

--- backtrack/backtrack.java
+++ backtrack/backtrack.java
@@ -29,6 +29,22 @@ class backtrack{
         //no choice
         subsets(str, ans, i+1);
     }
+    //fingind permutations it means all possibilities
+
+    public static void permu(String str,String ans){
+        //basecase
+        if(str.length()==0){System.out.println(ans);return;}
+        //recursion
+        for(int i=0;i<str.length();i++){
+          char curr=str.charAt(i);
+           
+          //"abcde"=>"ab"+"de"="abde"
+          String newstr=str.substring(0,i)+str.substring(i+1);
+
+          permu(newstr, ans+curr);
+
+        }
+    }
     public static void main(String args[]){
         int arr[]=new int[5];
          arrba(arr, 0, 1);
@@ -36,6 +52,9 @@ class backtrack{
          //subsets
          String str="abc";
          subsets(str, "", 0);
+
+         //permutation
+         permu(str, "");
 
     }
 }
