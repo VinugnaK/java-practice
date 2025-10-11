c23d95046e7bfe1d7b84d09a17987b7ff081b5e7
VinugnaK <152588912+VinugnaK@users.noreply.github.com>
2025-10-11 14:48:48 +0530

    Create backtrack2.java
    in nqueens if yes printing 1 sol,if not possible printing not possible

---
+++ backtrack/backtrack2.java
@@ -0,0 +1,82 @@
+class backtrack2{
+    public static boolean issafe(char board[][],int row,int col){
+        //vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        // diagonal left up
+         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+             if (board[i][j] == 'Q') {
+                    return false;
+                }
+          }
+
+        // diagonal right up
+          for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+              if (board[i][j] == 'Q') {
+                  return false;
+                }
+            }    
+        
+        return true;
+    }
+
+
+    //nqueens
+    public static boolean nqueens(char board[][],int row){
+        //base
+        if(row==board.length){
+            ////to count all possible ways
+           count++;
+           //to print all possible ways
+             ///////////////////////// printboard(board);
+            return true;
+        }
+        //column loop
+        
+        for(int i=0;i<board.length;i++){
+          if(issafe(board,row,i)){
+          board[row][i]='Q';
+          if(nqueens(board, row+1)){
+            return true;
+          }
+          board[row][i]='*';
+
+        }}
+        return false;
+    }
+    
+    public static void printboard(char board[][]){
+        System.out.println("----------chess board---------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+               System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+     static int count =  0; 
+    public static void main(String args[]){
+         //nqueens 
+         int n=5;
+         char board[][]=new char[n][n];
+         //intitialize
+         for(int i=0;i<n;i++){
+             for(int j=0;j<n;j++){
+                board[i][j]='*';
+             }
+         }
+         if(nqueens(board, 0)){
+            System.out.println("Solution is possible");
+            printboard(board);
+         }
+         else{
+             System.out.println("solution is not possible");
+         }
+         
+         
+
+    }
+}
