ca756321f9fca4e2925c857909747d29022f474d
VinugnaK <152588912+VinugnaK@users.noreply.github.com>
2025-10-12 12:11:16 +0530

    Update pairsum.java
    pair sum for sorted +rotated input

--- arrlis/pairsum.java
+++ arrlis/pairsum.java
@@ -1,6 +1,7 @@
 package arrlis;
 import java.util.*;
 public class pairsum {
+    //pair sum of sorted array
     public static boolean pair2p(ArrayList<Integer> List,int target){
         int lp=0;int rp=List.size()-1;
         while(lp<rp){
@@ -16,18 +17,40 @@ public class pairsum {
             }
 
         }return false;
+    } 
+    //pairsum of sorted+rotated array
+    public static boolean pairsumrot(ArrayList<Integer> List,int target){
+        int bp=-1;
+        for(int i=0;i<List.size();i++){
+            if(List.get(i)>List.get(i+1)){
+                bp=i;
+                break;
+            }
+        }
+
+        int lp=bp+1;
+        int rp=bp;
+        int n=List.size();
+        while(lp!=rp){
+            int sum=List.get(lp)+List.get(rp);
+           if(sum==target) return true;
+           if(sum<target) lp=(lp+1)%n;
+           else rp=(n+rp-1)%n;
+        }
+        return false;
     }
     public static void main(String args[]){
         ArrayList<Integer> List = new ArrayList<>();
+        List.add(4);
+        List.add(5);
         List.add(1);
         List.add(2);
         List.add(3);
-        List.add(5);
-        List.add(4);
-Collections.sort(List);
-        System.out.println(List);
-        int target=10;
-        System.out.println(pair2p(List,target));
+//Collections.sort(List);
+       // System.out.println(List);
+        int target=7;
+        //System.out.println(pair2p(List,target));
+        System.out.println(pairsumrot(List, target));
     }
     
 }
