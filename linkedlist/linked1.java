cc00e3cd71fc7f158f20aafc1741d9e1f816e952
VinugnaK <152588912+VinugnaK@users.noreply.github.com>
2025-10-13 19:11:39 +0530

    Create linked1.java
    linked list basics

---
+++ linkedlist/linked1.java
@@ -0,0 +1,22 @@
+package linkedlist;
+
+public class linked1 {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+
+    public static void main(String[] args) {
+     linked1 lin=new linked1();
+     lin.head=new Node(1);
+     lin.head.next=new Node(2);
+    }
+}
