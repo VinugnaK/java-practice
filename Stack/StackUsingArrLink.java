0a49f4c67b1ec8071ae4fa2624a60a2ada8ed681
VinugnaK <kodurilakshmivinugna@gmail.com>
2026-07-23 19:22:56 +0530

    Create StackUsingArrLink.java
    Stack using an arraylist

---
+++ Stack/StackUsingArrLink.java
@@ -0,0 +1,42 @@
+package Stack;
+
+import java.util.*;
+
+public class StackUsingArrLink {
+    static class Stack {
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isempty() {
+            return list.size() == 0;
+        }
+
+        public static void push(int data) {
+            list.add(data);
+        }
+
+        public static int pop() {
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        public static int peek() {
+            int top = list.get(list.size() - 1);
+            return top;
+        }
+    }
+
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(0);
+        s.push(1);
+        s.push(2);
+
+        while (!s.isempty()) {
+            System.out.println(s.peek());
+            s.pop();
+
+        }
+    }
+
+}
