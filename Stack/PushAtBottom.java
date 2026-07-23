f3cca2adc5670cc07ba404813a639900ed094942
VinugnaK <kodurilakshmivinugna@gmail.com>
2026-07-23 21:55:08 +0530

    Create PushAtBottom.java
    pushing the element at the bottom f the stack

---
+++ Stack/PushAtBottom.java
@@ -0,0 +1,29 @@
+//pushing the element at the bottom of the stack
+package Stack;
+
+import java.util.*;
+
+class PushAtBottom {
+    static void pushatbt(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushatbt(s, data);
+        s.push(top);
+    }
+
+    public static void main(String args[]) {
+        Stack<Integer> s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        pushatbt(s, 4);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+
+        }
+    }
+}
