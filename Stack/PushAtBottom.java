e7052e43bc885c7f73e80574439d7847654605c3
VinugnaK <kodurilakshmivinugna@gmail.com>
2026-07-25 09:18:25 +0530

    Update PushAtBottom.java
    reversing a stack using o(1) space complexity and used backtracking

--- Stack/PushAtBottom.java
+++ Stack/PushAtBottom.java
@@ -14,16 +14,42 @@ class PushAtBottom {
         s.push(top);
     }
 
+    public static void reversestack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reversestack(s);
+        pushatbt(s, top);
+    }
+
+    // beacuse of this line , once stack is printed it is poped out.so the stack
+    // becomes empty....and next time when we call/do an operation on stack, it is
+    // empty
+    public static void printstack(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+
+            System.out.println(s.pop());
+        }
+
+    }
+
     public static void main(String args[]) {
         Stack<Integer> s = new Stack();
         s.push(1);
         s.push(2);
         s.push(3);
         pushatbt(s, 4);
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
-
-        }
+        /*
+         * while (!s.isEmpty()) {
+         * System.out.println(s.peek());
+         * s.pop();
+         * 
+         * }
+         */
+        // comment one printstack and check it will work
+        // printstack(s);
+        reversestack(s);
+        printstack(s);
     }
 }
