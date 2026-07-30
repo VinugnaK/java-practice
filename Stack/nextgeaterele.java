f924dff76664979405e9f01d8009c64e7b9c4518
VinugnaK <kodurilakshmivinugna@gmail.com>
2026-07-30 11:42:20 +0530

    Update nextgeaterele.java
    max area of a rectangle in a histogram

--- Stack/nextgeaterele.java
+++ Stack/nextgeaterele.java
@@ -26,14 +26,69 @@ public class nextgeaterele {
 
     }
 
+    // to find the max are of the combination of rectangles in the histogram
+    static int maxAreaHist(int arr[]) {
+        int[] nsl = new int[arr.length];
+        int nsr[] = new int[arr.length];
+        int maxh = 0;
+
+        // next smallest right..same as ngr but change in while condition for ngr it is
+        // <= arr[i] but for nsl it is >=arr[i]
+        Stack<Integer> s = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                // -1
+                nsr[i] = arr.length;
+            } else {
+                // top ele
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+
+        }
+        // next smallest left
+        // empty the stack and start for nsr(same as nsl but starts from starting)
+        s = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                // -1
+                nsl[i] = -1;
+            } else {
+                // top ele
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+
+        }
+
+        // current area--width=j-i-1=nsr[i]=nsl[j]-1
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int area = height * width;
+            if (area > maxh) {
+                maxh = area;
+            }
+
+        }
+        return maxh;
+    }
+
     public static void main(String args[]) {
-        int[] arr = { 6, 8, 0, 1, 3 };
+        int[] arr = { 2, 1, 5, 6, 2, 3 };
         int[] greater = new int[arr.length];
         ng(arr, greater);
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(greater[i]);
+            // System.out.println(greater[i]);
 
         }
+        System.out.println(maxAreaHist(arr));
     }
 
 }
