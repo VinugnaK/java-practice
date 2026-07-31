8495db333097a8a008e11fb9d20ee102163b5f12
VinugnaK <kodurilakshmivinugna@gmail.com>
2026-07-31 18:30:31 +0530

    Update circularqueueusingarray.java
    circular queue using the array

--- Queue/circularqueueusingarray.java
+++ Queue/circularqueueusingarray.java
@@ -77,7 +77,10 @@ class circularqueueusingarray {
         q.add(1);
         q.add(2);
         q.add(3);
-        q.remove();
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
         while (!q.isEmpty()) {
             System.out.println(q.peek());
             q.remove();
