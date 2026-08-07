f3d269d786081dfd7874ae5ac5e78138935b9a36
VinugnaK <kodurilakshmivinugna@gmail.com>
2026-08-07 14:48:02 +0530

    Create queueReversal.java
    reversing a queue using the stack

---
+++ Queue/queueReversal.java
@@ -0,0 +1,32 @@
+package Queue;
+
+import java.util.*;
+
+public class queueReversal {
+    public static void reverse(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            s.push(q.remove());
+        }
+        for (int i = 0; i < size; i++) {
+            q.add(s.pop());
+        }
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        reverse(q);
+    }
+
+}
