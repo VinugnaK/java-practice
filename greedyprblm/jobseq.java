01cde49e2b0b16fcf0637ee77cd897190990d64a
VinugnaK <kodurilakshmivinugna@gmail.com>
2026-08-10 17:07:00 +0530

    Update jobseq.java
    order of jobs so that the we get max profit

--- greedyprblm/jobseq.java
+++ greedyprblm/jobseq.java
@@ -38,7 +38,27 @@ public class jobseq {
          * Job Deadline Profit AFTER SORTING
          * {1,40} 1 40
          * {1,30} 1 30
-         * {4,20} 4 20
+         * {4,20} 4 20 Ninety rupees Vietnamese coffee classic cold coffee platinum
+         * debit card is animals I mean almost six ninety nine seven sixty seven forty
+         * eight seven forty eight plus seventy six two forty nine hundred mL two
+         * professionals twenty packs fifty four cup noodles excluding list not less
+         * tomato chicken one thirty four plus seventy nine percent in total hundred
+         * rupees it's desert species bye bye one minute one minute for ten minutes
+         * twenty one day confirmation twenty one day straight some sexy type of dresses
+         * series but the thing is tutorial dance so contract person so much money worst
+         * job in the world very good I think I should be there it's so major related so
+         * definitely competition I think what are your strengths I respect your
+         * thoughts I am totally agreeing enjoy, so I wish someone by saying I know how
+         * to drive. never touched manual never I swear now which is not a good feeling
+         * and panic and and it's not nice Complete speak is in the media. but that's
+         * off accomplishment He's my best friend. They are already wearing the blouses.
+         * Outfit is outfit exactly. Like for me, that's a big thing. I don't really
+         * care. for me, I'm so glad to have expectation better company butter channel
+         * YouTube channel production team, we're back to love the family, just season
+         * fourteen season eight years back four plus six years back closest friend I
+         * try to enjoy I'm very expecting even last this long and see the cameras so I
+         * just wanna be done that ex fair competition I don't feel this is very fair
+         * and yes
          * {1,10} 1 10
          */
         // Find maximum deadline
