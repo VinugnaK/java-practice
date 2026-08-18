3a516b1ae7ce8c0eae26a5c9745b105bb87b12c5
VinugnaK <kodurilakshmivinugna@gmail.com>
2026-08-18 14:21:35 +0530

    Update height.java
    calculating diameter of a tree using 2 approaches

--- Binarytree/height.java
+++ Binarytree/height.java
@@ -39,6 +39,39 @@ class height {
             return lc + rc + root.data;
 
         }
+
+        // for calculating the diameter
+        public static int diameter(Node root) { // TC:O(N^2)
+            if (root == null) {
+                return 0;
+            }
+            int ldiam = diameter(root.left);
+            int rdiam = diameter(root.right);
+            int lhig = heigh(root.left);
+            int rhig = heigh(root.right);
+            int self = lhig + rhig + 1;
+            int ans = Math.max(ldiam, rdiam);
+            return Math.max(ans, self);
+        }
+
+        static class Info {
+            int dia, hei;
+
+            public Info(int dia, int hei) {
+                this.dia = dia;
+                this.hei = hei;
+            }
+        }
+
+        public static Info diamm(Node root) {// TTC: O(N)
+            if (root == null)
+                return new Info(0, 0);
+            Info leftinfo = diamm(root.left);
+            Info rightInfo = diamm(root.right);
+            int di = Math.max(leftinfo.dia, Math.max(rightInfo.dia, leftinfo.hei + rightInfo.hei + 1));
+            int ht = Math.max(leftinfo.hei, rightInfo.hei) + 1;
+            return new Info(di, ht);
+        }
     }
 
     public static void main(String[] args) {
@@ -58,6 +91,8 @@ class height {
         System.out.println(bt.heigh(root));
         System.out.println(bt.countofnode(root));
         System.out.println(bt.sumofnode(root));
-
+        System.out.println(bt.diameter(root));
+        System.out.println(bt.diamm(root).dia);
+        System.out.println(bt.diamm(root).hei);
     }
 }
